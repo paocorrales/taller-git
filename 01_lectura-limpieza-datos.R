adefac8b8b7c9d85fd3d8fcf9663b2846fecb94b
Elio Campitelli <eliocampitelli@gmail.com>
2025-08-27 21:26:32 +1000

    Agrega lectura de datos de temperatura

---
+++ 01_lectura-limpieza-datos.R
@@ -0,0 +1,7 @@
+# Carga de librerias
+library(readr)
+library(dplyr)
+
+# Lectura de datos 
+temperatura_raw <- read.csv("datos/temperatura_La-Pampa.csv") |>
+  filter(temp_max > 45) # filtro valores incorrectos
