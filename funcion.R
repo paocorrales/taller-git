ef765dc128465b3547fcc45689b8af36795d5997
Pao Corrales <paocorrales@users.noreply.github.com>
2025-10-03 19:24:32 +1000

    Update funcion.R

--- funcion.R
+++ funcion.R
@@ -1,1 +1,13 @@
 # Mi primera funcion
+
+pulgadas_a_centimetros <- function(medida_pulgadas) {
+  
+  if (!is.numeric(medida_pulgadas)) {
+    cli::cli_abort(c(
+      "medida_pulgadas debe ser numérca.",
+      "i" =  "La variable ingresada es un {class(medida_pulgadas)[1]}."
+    ))
+  }
+  
+  medida_pulgadas * 2.54
+}
